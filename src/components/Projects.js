daa7f6b250db4aae0c56c59fefba8e579b6cb914
kaixuannnn <kaixuant@yahoo.com>
2020-12-14 10:31:38 +0800

    modify

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,144 +1,214 @@
-import React from 'react';
-import styled from 'styled-components';
-import Toggle from './Toggle';
-import YelpCamp from '../assets/YelpCamp.PNG';
-import MusicPlayer from '../assets/MusicPlayer.PNG';
-import Ignite from '../assets/Ignite.PNG';
-import Capture from '../assets/Capture.PNG';
-import {motion,AnimateSharedLayout} from 'framer-motion';
-import { scrollReveal } from "../animation";
-import { useScroll } from "./useScroll";
+import React from 'react'
+import styled from 'styled-components'
+import Toggle from './Toggle'
+import YelpCamp from '../assets/YelpCamp.PNG'
+import MusicPlayer from '../assets/MusicPlayer.PNG'
+import Ignite from '../assets/Ignite.PNG'
+import Capture from '../assets/Capture.PNG'
+import { motion, AnimateSharedLayout } from 'framer-motion'
+import { scrollReveal } from '../animation'
+import { useScroll } from './useScroll'
 
-const Projects =()=>{
-    const [element, controls] = useScroll();
-    return(
-        <StyledProjects variants={scrollReveal}
-        animate={controls}
-        initial="hidden"
-        ref={element}>
-            <h1>My Projects</h1>
-            <AnimateSharedLayout>
-            <Toggle title="YelpCamp Application" description="A fullstack application mainly architected by HTML, CSS, NodeJS, ExpressJS, Passport, MongoDB and so on." picture={YelpCamp}>
-                <Details>
-                    <Tech>
-                        <p>JavaScript</p>
-                        <p>HTML</p>
-                        <p>CSS</p>
-                        <p>NodeJS</p>
-                        <p>ExpressJS</p>
-                        <p>MongoDB</p>
-                        <p>Mongoose</p>
-                        <p>MapBox</p>
-                    </Tech>
-                    <Links>
-                        <a href="https://github.com/kaixuannnn/bootcamp_yelpcamp" target="_blank" rel="noreferrer">GitHub</a>
-                        <a href="https://morning-harbor-95031.herokuapp.com/" target="_blank" rel="noreferrer">Go Live</a>
-                    </Links>
-                </Details>
-            </Toggle>
+const Projects = () => {
+  const [element, controls] = useScroll()
+  return (
+    <StyledProjects
+      variants={scrollReveal}
+      animate={controls}
+      initial='hidden'
+      ref={element}
+    >
+      <h1>My Projects</h1>
+      <AnimateSharedLayout>
+        <Toggle
+          title='YelpCamp Application'
+          description='A fullstack application mainly architected by HTML, CSS, NodeJS, ExpressJS, Passport, MongoDB and so on.'
+          picture={YelpCamp}
+          liveLink='https://morning-harbor-95031.herokuapp.com/'
+        >
+          <Details>
+            <Tech>
+              <p>JavaScript</p>
+              <p>HTML</p>
+              <p>CSS</p>
+              <p>NodeJS</p>
+              <p>ExpressJS</p>
+              <p>MongoDB</p>
+              <p>Mongoose</p>
+              <p>MapBox</p>
+            </Tech>
+            <Links>
+              <a
+                href='https://github.com/kaixuannnn/bootcamp_yelpcamp'
+                target='_blank'
+                rel='noreferrer'
+              >
+                GitHub
+              </a>
+              <a
+                href='https://morning-harbor-95031.herokuapp.com/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Go Live
+              </a>
+            </Links>
+          </Details>
+        </Toggle>
 
-            <Toggle title="Music Player" description="A mobile responsive music player application architected by React Hook, styiling with NodeSASS." picture={MusicPlayer}>
-                <Details>
-                    <Tech>
-                        <p>JavaScript</p>
-                        <p>ReactJS</p>
-                        <p>NodeSASS</p>
-                        <p>State Management</p>
-                        <p>React Hook</p>
-                    </Tech>
-                    <Links>
-                        <a href="https://github.com/kaixuannnn/musicplayer" target="_blank" rel="noreferrer">GitHub</a>
-                        <a href="https://kaixuannnn.github.io/musicplayer/" target="_blank" rel="noreferrer">Go Live</a>
-                    </Links>
-                </Details>
-            </Toggle>
- 
-            <Toggle title="Capture" description="A photographer portfolio website constructed by ReactJS framworks. Involved framer-motion animation, implemented React Route." picture={Capture}>
-                <Details>
-                <Tech>
-                        <p>JavaScript</p>
-                        <p>ReactJS</p>
-                        <p>StyledComponents</p>
-                        <p>State Management</p>
-                        <p>React Hook</p>
-                        <p>Framer Motion</p>
-                        <p>react-route-dom</p>
-                    </Tech>
-                    <Links>
-                        <a href="https://github.com/kaixuannnn/capture" target="_blank" rel="noreferrer">GitHub</a>
-                        <a href="https://kaixuannnn.github.io/capture/" target="_blank" rel="noreferrer">Go Live</a>
-                    </Links>
-                </Details>
-            </Toggle>
-            <Toggle title="Gaming Website" description="A gaming description websites built by ReactJS, fetching data from Rawg API,utilising Redux State Management." picture={Ignite}>
-                <Details>
-                <Tech>
-                        <p>JavaScript</p>
-                        <p>ReactJS</p>
-                        <p>StyledComponents</p>
-                        <p>Redux State Management</p>
-                        <p>React Hook</p>
-                        <p>Framer Motion</p>
-                        <p>react-route-dom</p>
-                        <p>Axios</p>
-                    </Tech>
-                    <Links>
-                        <a href="https://github.com/kaixuannnn/ignite" target="_blank" rel="noreferrer">GitHub</a>
-                        <a href="https://kaixuannnn.github.io/ignite/" target="_blank" rel="noreferrer">Go Live</a>
-                    </Links>
-                </Details>
-            </Toggle>
-            </AnimateSharedLayout>
-        </StyledProjects>
-    )
+        <Toggle
+          title='Music Player'
+          description='A mobile responsive music player application architected by React Hook, styiling with NodeSASS.'
+          picture={MusicPlayer}
+          liveLink='https://kaixuannnn.github.io/musicplayer/'
+        >
+          <Details>
+            <Tech>
+              <p>JavaScript</p>
+              <p>ReactJS</p>
+              <p>NodeSASS</p>
+              <p>State Management</p>
+              <p>React Hook</p>
+            </Tech>
+            <Links>
+              <a
+                href='https://github.com/kaixuannnn/musicplayer'
+                target='_blank'
+                rel='noreferrer'
+              >
+                GitHub
+              </a>
+              <a
+                href='https://kaixuannnn.github.io/musicplayer/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Go Live
+              </a>
+            </Links>
+          </Details>
+        </Toggle>
+
+        <Toggle
+          title='Capture'
+          description='A photographer portfolio website constructed by ReactJS framworks. Involved framer-motion animation, implemented React Route.'
+          picture={Capture}
+          liveLink='https://kaixuannnn.github.io/capture/'
+        >
+          <Details>
+            <Tech>
+              <p>JavaScript</p>
+              <p>ReactJS</p>
+              <p>StyledComponents</p>
+              <p>State Management</p>
+              <p>React Hook</p>
+              <p>Framer Motion</p>
+              <p>react-route-dom</p>
+            </Tech>
+            <Links>
+              <a
+                href='https://github.com/kaixuannnn/capture'
+                target='_blank'
+                rel='noreferrer'
+              >
+                GitHub
+              </a>
+              <a
+                href='https://kaixuannnn.github.io/capture/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Go Live
+              </a>
+            </Links>
+          </Details>
+        </Toggle>
+        <Toggle
+          title='Gaming Website'
+          description='A gaming description websites built by ReactJS, fetching data from Rawg API,utilising Redux State Management.'
+          picture={Ignite}
+          liveLink='https://kaixuannnn.github.io/ignite/'
+        >
+          <Details>
+            <Tech>
+              <p>JavaScript</p>
+              <p>ReactJS</p>
+              <p>StyledComponents</p>
+              <p>Redux State Management</p>
+              <p>React Hook</p>
+              <p>Framer Motion</p>
+              <p>react-route-dom</p>
+              <p>Axios</p>
+            </Tech>
+            <Links>
+              <a
+                href='https://github.com/kaixuannnn/ignite'
+                target='_blank'
+                rel='noreferrer'
+              >
+                GitHub
+              </a>
+              <a
+                href='https://kaixuannnn.github.io/ignite/'
+                target='_blank'
+                rel='noreferrer'
+              >
+                Go Live
+              </a>
+            </Links>
+          </Details>
+        </Toggle>
+      </AnimateSharedLayout>
+    </StyledProjects>
+  )
 }
 
 const StyledProjects = styled(motion.div)`
-    display: block;
-    padding: 5rem 10rem;
-    .line{
-        background: #858585;
-        height: 0.2rem;
-        margin: 1rem 0rem;
-        width: 100%;
-    }
-    @media screen and (max-width: 1051px){
+  display: block;
+  padding: 5rem 10rem;
+  .line {
+    background: #858585;
+    height: 0.2rem;
+    margin: 1rem 0rem;
+    width: 100%;
+  }
+  @media screen and (max-width: 1051px) {
     padding: 2rem 3rem;
-    }
-    @media screen and (max-width: 700px){
-        padding: 1rem;
-        font-size: 1.1rem;
-    }
-    `
+  }
+  @media screen and (max-width: 700px) {
+    padding: 1rem;
+    font-size: 1.1rem;
+  }
+`
 const Details = styled.div`
-    padding-top: 1.5rem;`
+  padding-top: 1.5rem;
+`
 const Tech = styled.div`
-    p{
-        font-size: 0.8rem;
-        display: inline-block;
-        padding: 0.5rem;
-        margin:0.5rem 0.3rem 0 0;
-        color: lightgrey;
-        background:#404040; 
-        border: solid 2px #404040;
-        border-radius: 10px;
-    }
-    `
+  p {
+    font-size: 0.8rem;
+    display: inline-block;
+    padding: 0.5rem;
+    margin: 0.5rem 0.3rem 0 0;
+    color: lightgrey;
+    background: #404040;
+    border: solid 2px #404040;
+    border-radius: 10px;
+  }
+`
 const Links = styled.div`
-    a{
-        /* display: inline-block;
+  a {
+    /* display: inline-block;
         text-decoration:none;
         color: #404040;
         background-color: white; */
-        padding: 1rem 1.5rem;
-        /* border: solid #404040 2px; */
-        margin: 1rem 5rem 1rem 0rem;
-        border-radius: 8px;
-        /* &:hover{
+    padding: 1rem 1.5rem;
+    /* border: solid #404040 2px; */
+    margin: 1rem 3rem 1rem 0rem;
+    border-radius: 8px;
+    /* &:hover{
         box-shadow: 2px 2px 4px grey;
         } */
-    } 
-
+  }
 `
 
-export default Projects;
+export default Projects
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,85 +1,98 @@
-import React,{useState} from 'react';
-import styled from 'styled-components';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faAngleDown, faAngleUp} from "@fortawesome/free-solid-svg-icons";
-import {motion} from 'framer-motion';
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleDown, faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import { motion } from 'framer-motion'
 
-const Toggle =({title, description, children,picture})=>{
-    const [toggle, setToggle] = useState(false);
-    const [hover, setHover] = useState(false);
-    return(
-        <Frame>
-        <Project>
-                <motion.div layout className="image" onMouseEnter={()=>setHover(true)}
-                    onMouseLeave={()=>setHover(false)}>
-                    <img className={toggle ? "afterToggle": ""} src={picture} alt=""/>
-                    {hover && 
-                    (<a href="/">Go Live</a>)}
-                </motion.div>
-                <motion.div layout className="des">
-                    <motion.h2 layout>{title}</motion.h2>
-                    <motion.p layout>{description}</motion.p>
-                    {toggle ? children : ""}
-                </motion.div>
-        </Project>
-        <motion.div layout className="showMore">
-            <FontAwesomeIcon classname="icon" icon={toggle ? faAngleUp : faAngleDown} size="lg" style={{cursor:"pointer"}} onClick={() => setToggle(!toggle)}/>
+const Toggle = ({ title, description, children, picture, liveLink }) => {
+  const [toggle, setToggle] = useState(false)
+  const [hover, setHover] = useState(false)
+  return (
+    <Frame>
+      <Project>
+        <motion.div
+          layout
+          className='image'
+          onMouseEnter={() => setHover(true)}
+          onMouseLeave={() => setHover(false)}
+        >
+          <img className={toggle ? 'afterToggle' : ''} src={picture} alt='' />
+          {hover && (
+            <a href={liveLink} target='_blank' rel='noreferrer'>
+              Go Live
+            </a>
+          )}
         </motion.div>
-        <motion.div layout className="line"></motion.div>
-        </Frame>
-
-    )
+        <motion.div layout className='des'>
+          <motion.h2 layout>{title}</motion.h2>
+          <motion.p layout>{description}</motion.p>
+          {toggle ? children : ''}
+        </motion.div>
+      </Project>
+      <motion.div layout className='showMore'>
+        <FontAwesomeIcon
+          classname='icon'
+          icon={toggle ? faAngleUp : faAngleDown}
+          size='lg'
+          style={{ cursor: 'pointer' }}
+          onClick={() => setToggle(!toggle)}
+        />
+      </motion.div>
+      <motion.div layout className='line'></motion.div>
+    </Frame>
+  )
 }
 const Frame = styled(motion.div)`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    .line{
-        background: #858585;
-        height: 0.2rem;
-        margin: 1rem 0rem;
-        width: 100%;
-    }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .line {
+    background: #858585;
+    height: 0.2rem;
+    margin: 1rem 0rem;
+    width: 100%;
+  }
 `
 const Project = styled(motion.div)`
-    display: flex;
-    .image{
-        flex:1;
-        min-height: 30vh;
-        overflow: hidden;
-        position: relative;
-        border-radius: 10px;
-        box-shadow: 0px 0px 8px lightgrey;
-    img { 
-        height: 100%;
-        width:100%;
-        object-fit:cover;
+  display: flex;
+  .image {
+    flex: 1;
+    min-height: 30vh;
+    overflow: hidden;
+    position: relative;
+    border-radius: 10px;
+    box-shadow: 0px 0px 8px lightgrey;
+    img {
+      height: 100%;
+      width: 100%;
+      object-fit: cover;
     }
     /* .afterToggle{
         height: 100% !important;
     } */
-    &:hover{
-        opacity: 0.7;
-        transition: all 0.5s ease;    }
-    a{
-        position:absolute;
-        z-index:3;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-
+    &:hover {
+      opacity: 0.7;
+      transition: all 0.5s ease;
     }
+    a {
+      position: absolute;
+      z-index: 3;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
     }
-    .des{
-        flex: 3;
-        padding: 1.5rem;
-        h2{
-            padding-top: 0px;
-        }
+  }
+  .des {
+    flex: 3;
+    padding: 1.5rem;
+    h2 {
+      padding-top: 0px;
     }
-    @media screen and (max-width: 700px){
-        .image{
-            display:none
-        }}
+  }
+  @media screen and (max-width: 700px) {
+    .image {
+      display: none;
+    }
+  }
 `
-export default Toggle;
+export default Toggle
